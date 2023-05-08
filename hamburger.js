f15576f3a3c726e3386793268bd44aa35687bf86
gahank <gahank@oregonstate.edu>
2023-05-07 19:59:59 -0700

    updated header formatting

--- hamburger.js
+++ hamburger.js
@@ -1,13 +1,19 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const navreg = document.querySelector(".nav-regular");
+const navscrolled = document.querySelector(".nav-scrolled");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    navreg.classList.toggle("menu-on");
+    navscrolled.classList.toggle("menu-on");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.
     addEventListener("click", () => {
-        hamburger.classList.remove("active")
-        navMenu.classList.remove("active")
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+        navreg.classList.remove("menu-on");
+        navscrolled.classList.remove("menu-on");
     }))
